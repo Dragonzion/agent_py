4e34117239e24801519ae9b51df3e8544c757a00
Dragonzion <alexandre02a@hotmail.com>
2026-09-11 21:44:53 +0100

    testing scope working dir

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,7 +8,13 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         if os.path.commonpath([working_path, target_dir]) != working_path:
             return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
         if os.path.isdir(target_dir):
-            return (f'Success: "{directory}" is within the working directory')
+            dir_list = os.listdir(target_dir)
+            contents_lines = []
+            for item in dir_list:
+                item_path = os.path.join(target_dir, item)
+                new_line = f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}"
+                contents_lines.append(new_line)
+            return ("\n".join(contents_lines))
         else:
             return (f'Error: "{directory}" is not a directory')
     except Exception as e: #noqa
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,9 +1,9 @@
 from functions.get_files_info import get_files_info
 
 print(
-get_files_info("calculator", "."),
-get_files_info("calculator", "/bin"),
-get_files_info("calculator", "../"),
-get_files_info("calculator", "main.py"),
+"Result for current directory:" + "\n  " + get_files_info("calculator", ".").replace("\n", "\n  "),
+"Result for 'pkg' directory:" +  "\n  " + get_files_info("calculator", "pkg").replace("\n", "\n  "),
+"Result for '/bin' directory:" +  "\n  " + get_files_info("calculator", "/bin").replace("\n", "\n  "),
+"Result for '../' directory:" +  "\n  " + get_files_info("calculator", "../").replace("\n", "\n  "),
 sep="\n"
 )
